ac597c3ed5c06e94b9739b5ce4c2a6478d4a6f1e
Game Dev Daily <69360222+weerapan-sub@users.noreply.github.com>
2023-06-14 20:14:13 +0700

    Update main.js

--- main.js
+++ main.js
@@ -17,13 +17,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const raccoon = await loadGLTF("./applications/assets/models/donald_trump_dancing/scene.gltf");
     raccoon.scene.scale.set(1, 1, 1);
-    raccoon.scene.position.set(0, -0.4, 0);
-    //raccoon.scene.rotation.set(90 ,0, 0);
+    raccoon.scene.position.set(0, 0, 0);
+    raccoon.scene.rotation.set(90 ,0, 0);
 
     const bear = await loadGLTF("./applications/assets/models/joe_biden_dancing/scene.gltf");
     bear.scene.scale.set(1, 1, 1);
-    bear.scene.position.set(0, -0.4, 0);
-    //bear.scene.rotation.set(90 ,0, 0);
+    bear.scene.position.set(0, 0, 0);
+    bear.scene.rotation.set(90 ,0, 0);
 
     const raccoonAnchor = mindarThree.addAnchor(0);
     raccoonAnchor.group.add(raccoon.scene);
